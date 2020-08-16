e6ed2a09af9cb58f066250c3b141e878cab02c23
lukaszKielar <kielar.lukasz@hotmail.com>
2020-08-16 19:28:16 +0200

    fix issues

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -58,7 +58,6 @@ mod tests {
     fn create_international_package() {
         let sender_country = String::from("Spain");
         let recipient_country = String::from("Russia");
-
         let package = Package::new(sender_country, recipient_country, 1200);
 
         assert!(package.is_international());
@@ -68,7 +67,6 @@ mod tests {
     fn calculate_transport_fees() {
         let sender_country = String::from("Spain");
         let recipient_country = String::from("Spain");
-
         let cents_per_kg = 3000;
         let package = Package::new(sender_country, recipient_country, 1500);
 
