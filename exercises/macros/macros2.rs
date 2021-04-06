e5b919adae4cc6dc38a3f723411f9da963b2b383
lukaszKielar <kielar.lukasz@hotmail.com>
2021-04-06 22:36:54 +0200

    fix macros

--- exercises/macros/macros2.rs
+++ exercises/macros/macros2.rs
@@ -1,12 +1,13 @@
 // macros2.rs
 // Make me compile! Execute `rustlings hint macros2` for hints :)
 
+fn main() {
+    my_macro!();
+}
+
+#[macro_export]
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
     };
-}
-
-fn main() {
-    my_macro!();
 }
